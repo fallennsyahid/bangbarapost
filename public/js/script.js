9e18ae489b33e96b08ac986d44b1e06777e019a4
fallennsyahid <umarusyahidmasudi1@gmail.com>
2025-01-14 20:21:47 +0700

    update menu

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,22 @@ window.onscroll = function () {
 }
 
 
+// Menu Active
+const menuLinks = document.querySelectorAll('.menu-link');
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Mencegah aksi default link
+
+        // Hapus kelas active dari semua link
+        menuLinks.forEach(l => l.classList.remove('active'));
+
+        // Tambahkan kelas active ke link yang diklik
+        this.classList.add('active');
+    });
+});
+
+
 
 const hamburger = document.getElementById('#hamburger');
 const navMenu = document.getElementById('#nav-menu');
@@ -18,3 +34,6 @@ const navMenu = document.getElementById('#nav-menu');
 hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('hidden');
 });
+
+
+
