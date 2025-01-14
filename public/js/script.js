a7dff994255b705d2d48179ec0c4b7aa746a3737
fallennsyahid <umarusyahidmasudi1@gmail.com>
2025-01-14 17:14:31 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,14 @@
 // Navbar Fixed 
+window.onscroll = function () {
+    const navbarNav = document.querySelector('#navbar');
+    const fixedNav = navbarNav.offsetTop;
+
+    if (window.scrollY > fixedNav) {
+        navbarNav.classList.add('navbar-fixed');
+    } else {
+        navbarNav.classList.remove('navbar-fixed');
+    }
+}
 
 
 
